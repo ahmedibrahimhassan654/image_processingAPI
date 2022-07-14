a11684c947a09707ccf20b427ba5655f7cec77cd
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-07-14 02:42:51 +0200

    install express server

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,19 @@
-/** Use Axios to get data from restcountries api */
-import axios from 'axios';
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 
-/** Use the free API https://restcountries.eu/
- * You will use the following endpoints
- * https://restcountries.eu/rest/v2/name/{name} for countries by name
- * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
- */
-
-/** Create getCountry Function here */
-async function getCountry(name: string) {
-  const getApi = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
-  const data = getApi.data[0];
-  return {
-    capital: data.capital,
-    region: data.region,
-    numericCode: data.numericCode,
-  };
+const app = express();
+// Load env vars
+dotenv.config({ path: './config.env' });
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-/** Create a test for this getRegion function */
-async function getRegionCountries(regionalbloc: string) {
-  const getApi = await axios(
-    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
-  );
-  const data = getApi.data;
-  const countries = [];
-  for (let i = 0; i < data.length; i++) {
-    countries.push(data[i].name);
-  }
-  return countries;
-}
-
-/** Create getRegionCapitals function here */
-async function getRegionCapitals(regionalbloc: string) {
-  const countryEndpoints = await getRegionCountries(regionalbloc);
-  const promises = countryEndpoints.map((endpoint: any) =>
-    axios(`https://restcountries.eu/rest/v2/name/${endpoint}`)
-  );
-  const arr = await Promise.all(promises);
-  const capitals = [];
-  for (let i = 0; i < arr.length; i++) {
-    capitals.push(arr[i].data[0].capital);
-  }
-  return capitals;
-}
-
-export default {
-  getCountry,
-  getRegionCountries,
-  getRegionCapitals,
-};
+const PORT = process.env.PORT || 5000;
+app.get('/api', (req, res) => {
+  res.send('Hello, world!');
+});
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
